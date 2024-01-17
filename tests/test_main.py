ff2a0433c38cd5f1a577c47546bda75f5f3539d8
Roland Guichard <roland.guichard@pasqal.com>
2024-01-17 21:34:06 +0000

    Remove obsolete tests.

--- tests/test_main.py
+++ tests/test_main.py
@@ -13,22 +13,8 @@ from qadence.measurements import Measurements
 from qadence.noise.protocols import Noise
 from qadence.operations import CNOT, RX, Z
 from qadence.types import BackendName
-from template_python.main import main
 
 from qadence_protocols.twirl_mitigation import twirl_mitigation
-
-expected_msg = "Pasqal template Python project"
-
-
-def test_main() -> None:
-    msg = main()
-    assert msg == expected_msg
-
-
-def test_main_with_str() -> None:
-    str_to_add = "with added str"
-    msg = main(str_to_add=str_to_add)
-    assert msg == expected_msg + str_to_add
 
 
 @pytest.mark.parametrize(
