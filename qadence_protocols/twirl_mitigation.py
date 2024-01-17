ed886f0db47303914c4630bb70b41ef8a827455e
raja_selvarajan <raja.selvarajan@pasqal.com>
2024-01-17 18:50:33 +0100

    model free readout mitigation using twirl

---
+++ qadence_protocols/twirl_mitigation.py
@@ -0,0 +1,105 @@
+from __future__ import annotations
+
+from collections import Counter
+
+# import pytest
+import torch
+# from metrics import MIDDLE_ACCEPTANCE
+from torch import Tensor
+
+from qadence import (
+    QuantumCircuit,
+    QuantumModel,
+    chain,
+)
+from qadence.noise.protocols import Noise
+from qadence.operations import  X,  kron
+from qadence.types import BackendName
+import itertools
+from functools import reduce
+import numpy as np
+from functools import reduce
+
+
+def twirl_swap(
+    n_qubits:int,
+    twirl: tuple,
+    samples_twirl: dict
+) -> dict:
+    
+    output ={}
+    operand =  ''.join(map(str,[1 if i in twirl else 0 for i in range(n_qubits)]))
+    for key in samples_twirl.keys():
+        output['{0:b}'.format(int(key,2)^int(operand,2))] = samples_twirl[key]
+
+    return output
+
+
+
+def compute_exp(
+    n_qubits: int,
+    samples_twirl_list: list, 
+    observable: list
+) -> float:
+    
+    out:float = 0
+    Z_mat = np.eye(2)
+    Z_mat[1,1]= -1
+    o = reduce(np.kron,[Z_mat if i in observable else np.eye(2) for i in range(n_qubits)])
+
+    for samples_twirl in samples_twirl_list:
+        shots = sum(samples_twirl.values())
+        for key in samples_twirl.keys():
+            out += o[int(key,2),int(key,2)]*samples_twirl[key]/shots
+    return out
+
+
+
+def twirl_mitigation(
+    n_qubits: int,
+    circuit: QuantumCircuit,
+    backend: BackendName, 
+    noise: Noise,
+    n_shots: int,
+    observable: list
+) -> float:
+
+    ## check if observable is in the Z basis
+    ## extend protocol to sum of Z basis observables  
+    ### by default lets do all twirling and specialize later for user inputs
+
+    twirl_list = list(itertools.chain.from_iterable([list(itertools.combinations(range(n_qubits), k)) for k in range(1,n_qubits)]))
+
+
+    ###generate samples for all twirls of circuit
+    samples_twirl_num_list = []
+    samples_twirl_den_list = []
+    for twirl in twirl_list:
+        layer = [X(i) for i in twirl]
+        block_twirl = chain(circuit.block,kron(*layer))
+        
+        circ_twirl_num = QuantumCircuit(block_twirl.n_qubits,block_twirl)
+        model_twirl_num = QuantumModel(circuit=circ_twirl_num, backend=BackendName.PYQTORCH)
+        samples_twirl_num = model_twirl_num.sample(noise=noise, n_shots=n_shots)[0]
+
+        samples_twirl_num = twirl_swap(n_qubits,twirl,samples_twirl_num)
+        samples_twirl_num_list.append(samples_twirl_num)
+
+        circ_twirl_den = QuantumCircuit(block_twirl.n_qubits,kron(*layer))
+        model_twirl_den = QuantumModel(circuit=circ_twirl_den, backend=backend)
+        samples_twirl_den = model_twirl_den.sample(noise=noise, n_shots=n_shots)[0]
+        
+        samples_twirl_den = twirl_swap(n_qubits,twirl,samples_twirl_den)
+        samples_twirl_den_list.append(samples_twirl_den)         
+
+
+    expectation: float = 0 
+
+    for o in observable:
+        expectation_num = compute_exp(n_qubits,samples_twirl_num_list, o)
+        expectation_den = compute_exp(n_qubits,samples_twirl_den_list, o)
+        expectation += expectation_num/expectation_den
+
+
+    return expectation
+
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,21 @@
 from __future__ import annotations
 
+import pytest
+import torch
+from qadence import (
+    AbstractBlock,
+    QuantumCircuit,
+    QuantumModel,
+    chain,
+    kron,
+)
+from qadence.measurements import Measurements
+from qadence.noise.protocols import Noise
+from qadence.operations import CNOT, RX, Z
+from qadence.types import BackendName
 from template_python.main import main
+
+from qadence_protocols.twirl_mitigation import twirl_mitigation
 
 expected_msg = "Pasqal template Python project"
 
@@ -14,3 +29,55 @@ def test_main_with_str() -> None:
     str_to_add = "with added str"
     msg = main(str_to_add=str_to_add)
     assert msg == expected_msg + str_to_add
+
+
+@pytest.mark.parametrize(
+    "error_probability, n_shots, block, backend, optimization_type",
+    [
+        (
+            0.2,
+            100000,
+            2,
+            chain(kron(RX(0, torch.pi / 3), RX(1, torch.pi / 3)), CNOT(0, 1)),
+            [[0, 1], [1]],
+            BackendName.PYQTORCH,
+        )
+    ],
+)
+def test_readout_mitigation_quantum_model(
+    error_probability: float,
+    n_shots: int,
+    n_qubits: int,
+    block: AbstractBlock,
+    observable: list,
+    backend: BackendName,
+) -> None:
+
+
+    circuit = QuantumCircuit(block.n_qubits, block)
+    Z_obs = sum([kron(*[Z(i) for i in a]) for a in observable])
+
+    noise = Noise(protocol=Noise.READOUT, options={"error_probability": error_probability})
+    tomo_measurement = Measurements(
+        protocol=Measurements.TOMOGRAPHY,
+        options={"n_shots": n_shots},
+    )
+
+    model = QuantumModel(
+        circuit=circuit,
+        backend=BackendName.PYQTORCH,
+        diff_mode="gpsr",
+        observable=Z_obs,
+        measurement=tomo_measurement,
+    )
+
+    expectation_noisless = model.expectation(
+        measurement=tomo_measurement,
+    )
+    # expectation_noisy = model.expectation(measurement=tomo_measurement,noise=noise)
+
+    expectation_mitigated = twirl_mitigation(n_qubits, circuit, backend, noise, n_shots, observable)
+
+    # print(expectation_noisless,expectation_noisy,expectation_mitigated)
+
+    assert torch.allclose(expectation_mitigated, expectation_noisless, atol=1.0e-1)
