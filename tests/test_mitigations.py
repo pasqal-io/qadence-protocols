f96e298f8ab0b9b83b3cd45670c4164a6449c20c
Roland-djee <9250798+Roland-djee@users.noreply.github.com>
2024-01-29 16:51:15 +0000

    Model free readout error mitigation (#3)
    * model free readout mitigation using twirl
    
    * Fix lint.
    
    * CI files.
    
    * Delete unnecessary file.
    
    * Clean up more files.
    
    * Correct syntax.
    
    * Adapt action to repo.
    
    * Remove obsolete tests.
    
    * Reorganise repo.
    
    * Trigger CI tests correctly.
    
    * Update pre-commits.
    
    * Correct import.
    
    * Correct import.
    
    * fixed test case parameters
    
    * fixed linting
    
    * fixing names and return types
    
    * fixing return
    
    ---------
    
    Co-authored-by: raja_selvarajan <raja.selvarajan@pasqal.com>

---
+++ tests/test_mitigations.py
@@ -0,0 +1,67 @@
+from __future__ import annotations
+
+import pytest
+import torch
+from qadence import (
+    AbstractBlock,
+    QuantumCircuit,
+    QuantumModel,
+    chain,
+    kron,
+)
+from qadence.measurements import Measurements
+from qadence.noise.protocols import Noise
+from qadence.operations import CNOT, RX, Z
+from qadence.types import BackendName
+
+from qadence_protocols.mitigations.twirl import twirl_mitigation
+
+# from qadence_protocols.twirl_mitigation import twirl_mitigation
+
+
+@pytest.mark.parametrize(
+    "error_probability, n_shots, n_qubits, block, observable, backend",
+    [
+        (
+            0.2,
+            10000,
+            2,
+            chain(kron(RX(0, torch.pi / 3), RX(1, torch.pi / 3)), CNOT(0, 1)),
+            [[0, 1], [1]],
+            BackendName.PYQTORCH,
+        )
+    ],
+)
+def test_readout_twirl_mitigation(
+    error_probability: float,
+    n_shots: int,
+    n_qubits: int,
+    block: AbstractBlock,
+    observable: list,
+    backend: BackendName,
+) -> None:
+    circuit = QuantumCircuit(block.n_qubits, block)
+    Z_obs = sum([kron(*[Z(i) for i in a]) for a in observable])
+
+    noise = Noise(protocol=Noise.READOUT, options={"error_probability": error_probability})
+    tomo_measurement = Measurements(
+        protocol=Measurements.TOMOGRAPHY,
+        options={"n_shots": n_shots},
+    )
+
+    model = QuantumModel(
+        circuit=circuit,
+        backend=BackendName.PYQTORCH,
+        diff_mode="gpsr",
+        observable=Z_obs,
+        measurement=tomo_measurement,
+    )
+
+    expectation_noisless = model.expectation(
+        measurement=tomo_measurement,
+    )
+    # expectation_noisy = model.expectation(measurement=tomo_measurement,noise=noise)
+
+    expectation_mitigated = twirl_mitigation(n_qubits, circuit, backend, noise, n_shots, observable)
+
+    assert torch.allclose(expectation_mitigated, expectation_noisless, atol=1.0e-2, rtol=1.0e-1)
